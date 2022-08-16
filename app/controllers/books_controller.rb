465c711e2618771723bfd0758c42f9ee0321f49c
EC2 Default User <ec2-user@ip-172-31-4-68.ap-northeast-1.compute.internal>
2022-08-16 08:49:07 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,55 @@
 class BooksController < ApplicationController
+  protect_from_forgery :except => [:destroy]
+  
   def index
-    puts 'a'
+    @books = Book.all
+    @book = Book.new
   end
 
-  def new
+  def create
+    # １. データを新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    if @book.save
+      flash[:success] = 'Book was successfully created.'
+      redirect_to @book
+    else
+      @books = Book.all
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    
+    if @book.update(book_params)
+      flash[:success] = 'Book was successfully updated.'
+      redirect_to @book
+    else
+      @books = Book.all
+      render :edit, status: :unprocessable_entity
+    end
+  end  
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:success] = 'Book was successfully destroyed.'
+    redirect_to books_path, status: :see_other
+  end  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  # ---- 以下を追加してください ---- #
+  validates :title, presence: true
+  validates :body, presence: true
 end
